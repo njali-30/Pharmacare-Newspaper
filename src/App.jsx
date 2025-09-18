63e53e94bac8e18f69cf126b2248f90f4d928ae9
Bajaj Manjit <145859515+Bajajmanjit12@users.noreply.github.com>
2025-09-18 21:51:23 +0530

    New Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,30 @@ import Contact from "./ContactUs";
 import Admin from "./Admin";
 import "./App.css";
 
+// A wrapper for homepage that contains all sections in one scrollable page
+function HomePage() {
+  return (
+    <div>
+      <div id="home"><Home /></div>
+      <div id="about"><About /></div>
+      <div id="editions"><Editions /></div>
+      <div id="specials"><Specials /></div>
+      <div id="contact"><Contact /></div>
+    </div>
+  );
+}
+
 export default function App() {
-    return (
-        <Router>
-            <Navbar />
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        {/* Scrollable one-page homepage */}
+        <Route path="/" element={<HomePage />} />
 
-            {/* Scrollable homepage sections */}
-            <div id="home"><Home /></div>
-            <div id="about"><About /></div>
-            <div id="editions"><Editions /></div>
-            <div id="specials"><Specials /></div>
-            <div id="contact"><Contact /></div>
-
-
-
-        </Router>
-    );
+        {/* Separate pages */}
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </Router>
+  );
 }
